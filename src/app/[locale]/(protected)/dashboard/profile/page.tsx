fe752f55da01e646c6f52e692bdb648308fd021d
nab1h <nabih_ahmed2563@yahoo.com>
2026-09-13 14:14:41 -0700

    do

--- src/app/[locale]/(protected)/dashboard/profile/page.tsx
+++ src/app/[locale]/(protected)/dashboard/profile/page.tsx
@@ -3,15 +3,22 @@ import { ChangePasswordForm } from "../_components/change-password-form";
 import { ProfileForm } from "../_components/profile-form";
 
 export default function ProfilePage() {
-  const t = useTranslations("Dashboard");
+  const t = useTranslations("profile");
+
   return (
     <>
       <div className="flex w-full flex-col space-y-8 p-4 sm:px-6 lg:px-8">
         <div className="space-y-2 text-start">
-          <h1 className="font-medium text-3xl">{t("title")}</h1>
-          <p className="text-muted-foreground text-sm">Please enter your details to update your profile.</p>
+          <h1 className="font-medium text-3xl">
+            {t("title")}
+          </h1>
+
+          <p className="text-muted-foreground text-sm">
+            {t("description")}
+          </p>
         </div>
-        <div className="w-full space-y-4 ">
+
+        <div className="w-full space-y-4">
           <ProfileForm />
           <ChangePasswordForm />
         </div>
